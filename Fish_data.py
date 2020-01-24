3ca12f7cc4593fdab9a77ebfdeb0df7129470570
DanaeCS <danae.stephens@gmail.com>
2020-01-24 13:39:46 -0700

    updateded raw data parser

--- Fish_data.py
+++ Fish_data.py
@@ -15,3 +15,10 @@ input_name = "BIO365-FishData.xlsx"
 
 
 data_frame = pd.read_excel(input_name)
+
+column_headers = ["FishID","Date", "TREND","Gear", "Species", "Gender", "Length", 
+                  "Mass", "Ktl", "Relative weight", "Maturity", "Age structure",
+                  "stomach", "gonads", "fat_index", "parasite", "misc 1 text",
+                  "misc 2 num", "misc 3 text", "misc 4 num", "Site", "KFL"]
+
+data_frame.columns = column_headers
