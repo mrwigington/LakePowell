0c58972603e78211171ba51b22fc10b943f6c873
hboekweg <hannahboekweg@gmail.com>
2020-01-29 09:58:55 -0700

    fish data download functional

--- Fish_data.py
+++ Fish_data.py
@@ -9,9 +9,29 @@ Created on Wed Jan 15 13:21:03 2020
 ### this is how to defien a class
 
 import pandas as pd
+import download
 
 
-input_name = "BIO365-FishData.xlsx"
+class Fish():
+    def __init__(self):
+        #initialize a dictionary or somthing to hold dataframes
+        self.dataframes = {}
+        #get the fish data
+        input_name = download.download_fish_file()
+        #build the fish data parsar
+        dataframe = pd.read_excel(input_name)
+        
+        self.dataframes["fish_data"] = dataframe
 
 
-data_frame = pd.read_excel(input_name)
+        #get the water data
+        #build the water parsar
+
+        # import pdb; pdb.set_trace()
+
+
+    def get_fish_data(self):
+        return self.dataframes["fish_data"]
+
+    def get_water_data(self):
+        pass
