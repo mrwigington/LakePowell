b550d47f6b90ad2a6cc3129d40231715046d8da8
DanaeCS <danae.stephens@gmail.com>
2020-02-19 13:57:01 -0700

    function outline

---
+++ correleation_functions.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb  7 13:34:44 2020
+
+@author: aquag
+"""
+from scipy.stats.stats import pearsonr
+#import numpy as np
+
+#need to pull the fish data
+    #what are we looking at? catch erp unit effort? biomass?
+#pull the water levels
+#find corelation
+def cor_min_height(fish_data, water_data):
+    corelation = []
+    return corelation
+
+def cor_max_height(fish_data, water_data):
+    corelation = []
+    return corelation
+
+def cor_ave_depth(fish_data, water_data):
+    corelation = []
+    return corelation
+
+def cor_change_height(fish_data, water_data):
+    corelation = []
+    return corelation
+
+#additinal funciton that will find best match for a set range of years
+#will call one of the smaller functions to do so
