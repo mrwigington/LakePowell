6116eb1dafeca08ed3cb3911684e898e60058a1b
hboekweg <hannahboekweg@gmail.com>
2020-03-03 22:04:24 -0700

    restructure for Pypi release

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name = 'lakepowell',
+    packages = ['lakepowell'],
+    version = '0.1.1',  # Ideally should be same as your GitHub release tag varsion
+    description = 'description',
+    author = '',
+    author_email = '',
+    url = 'https://github.com/hboekweg/fishdata.git',
+    download_url = 'download link you saved',
+    keywords = ['tag1', 'tag2'],
+    classifiers = [],
+)
