7012e72463f2c9689b93f76289f94cc8f456eeb2
Matthew Wigington <mrwigington@gmail.com>
2020-03-26 23:14:48 -0600

    added comments

--- lakepowell/cleaner.py
+++ lakepowell/cleaner.py
@@ -8,13 +8,14 @@ class Cleaner():
     # initialize filter object with pandas data frame
     def __init__(self, fish_df):
         self.fish_df = fish_df.copy() #pandas data frame copied to avoid pointer problems
-        self.dirty_fish_id = []
-        self.dirty_fish = []
-        self.dirty_idx = []
+        self.dirty_fish_id = [] #ids of fish with errors in its row
+        self.dirty_fish = [] #all rows with errors
+        self.dirty_idx = [] #idx of rows with errors (for quick look up and removal)
 
+    #returns all of the erroneous rows found while cleaning the fish data
     def get_dirty_data(self):
         column_headers = ["FishID","Date", "TREND","Gear", "Species", "Sex", "Length",
-                          "Mass", "Ktl", "Weight", "Maturity", "Age structure",
+                          "Mass", "Ktl", "Mass", "Maturity", "Age structure",
                           "stomach", "gonads", "fat_index", "parasite", "misc 1 text",
                           "misc 2 num", "misc 3 text", "misc 4 num", "Site", "KFL"]
         dirty_df = pd.DataFrame.from_records(self.dirty_fish)
@@ -22,6 +23,7 @@ class Cleaner():
 
         return dirty_df
 
+    #converts all categorical variable abbreviations (Species, Site, Gear, Sex) to upper case and removes white space
     def prep_data(self):
         self.fish_df['Species'] = self.fish_df['Species'].str.upper()
         self.fish_df['Site'] = self.fish_df['Site'].str.upper()
@@ -32,6 +34,7 @@ class Cleaner():
         self.fish_df['Gear'] = self.fish_df['Gear'].str.replace(" ", "")
         self.fish_df['Sex'] = self.fish_df['Sex'].str.replace(" ", "")
 
+    #cleans the fish data based on the booleans indicating which columns should be checked for errors
     def clean_fish_data(self, date = True, mass = True, length = True, species = True,
                         site = True, gear = True, sex = True):
         self.prep_data()
@@ -53,7 +56,7 @@ class Cleaner():
                 if sex:
                     self.check_sex(i)
 
-            except:
+            except: #catches the errors thrown by individual cleaning functions and adds those rows to error lists
                 self.dirty_fish_id.append(self.fish_df["FishID"][i])
                 self.dirty_fish.append(list(self.fish_df.iloc[i,]))
                 self.dirty_idx.append(i)
@@ -61,9 +64,10 @@ class Cleaner():
         # print(self.dirty_fish_id)
         # print(pd.DataFrame.from_records(self.dirty_fish))
         # print(len(self.dirty_idx))
-
+        # self.fish_df.replace(0, np.nan, inplace=True)
         return self.fish_df.drop(self.dirty_idx)
 
+    #checks to make sure there are no date errors at index i
     def check_date(self, i):
         cur_year = date.today().year
         date_caught = self.fish_df["Date"][i]
@@ -72,13 +76,14 @@ class Cleaner():
         month = date_caught.month
         year = date_caught.year
 
-        if (year < 1962) or (year > cur_year):
+        if (year < 1962) or (year > cur_year):#there shouldn't be any records before filling of lake and today's date
             raise ValueError('Year not in range: ', year)
         elif (month < 1) or (month > 12):
             raise ValueError('Month not in range: ', month)
-        elif (day < 1) or (day > 31):
+        elif (day < 1) or (day > 31):#not a robust check, but should be sufficient
             raise ValueError('Day not in range: ', day)
 
+    #checks to make sure there are no species abreviation errors at index i
     def check_species(self, i):
         species = self.fish_df["Species"][i]
         valid_species = ["GSF", "STB", "BGL", "SMB", "LMB", "WAE", "GZD", "BLC",
@@ -88,6 +93,7 @@ class Cleaner():
         if species not in valid_species:
             raise ValueError('Invalid species: ', species)
 
+    #checks to make sure there are no gear abreviation errors at index i
     def check_gear(self, i):
         gear = self.fish_df["Gear"][i]
         valid_gear = ["GN", "EL", "AN", "CR", "SN"]
@@ -95,6 +101,7 @@ class Cleaner():
         if gear not in valid_gear:
             raise ValueError('Invalid gear: ', gear)
 
+    #checks to make sure there are no sex abreviation errors at index i
     def check_sex(self, i):
         sex = self.fish_df["Sex"][i]
         valid_sexes = ["U", "F", "M"]
@@ -102,6 +109,7 @@ class Cleaner():
         if (sex not in valid_sexes) and (not np.isnan(sex)):
             raise ValueError('Invalid sex: ', sex)
 
+    #checks to make sure fish at length index i, if recorded, is not a negative number
     def check_length(self, i):
         length = self.fish_df["Length"][i]
 
@@ -109,6 +117,7 @@ class Cleaner():
             if length < 0:
                 raise ValueError('Invalid length (< 0): ', length)
 
+    #checks to make sure fish mass at index i, if recorded, is not a negative number
     def check_mass(self, i):
         mass = self.fish_df["Mass"][i]
 
@@ -116,10 +125,13 @@ class Cleaner():
             if mass < 0:
                 raise ValueError('Invalid mass (< 0): ', mass)
 
+    #checks to make sure there are no site abreviation errors at index i
     def check_site(self, i):
         site = self.fish_df["Site"][i]
-        valid_site = ['GH', 'WW', 'SJ', 'BF', 'PB', 'RN', 'WC', 'AT', 'HA','NW',
-                        'HC', 'CR', 'GB', 'DM', 'LC', 'NK', 'AI', 'RC', 'HI', 'KC']
+        #these were ones given by Alexandria
+        # valid_site = ['GH', 'WW', 'SJ', 'BF', 'PB', 'RN', 'WC', 'AT', 'HA','NW',
+        #                 'HC', 'CR', 'GB', 'DM', 'LC', 'NK', 'AI', 'RC', 'HI', 'KC']
+        #these were ones with frequences high enough to probably be real
         valid_site = ["GH", "RN", "SJ", "WW", "BF", "PB", "KC", "NW", "HA",
                             "PF", "HC", "WC", "CC", "SC", "DM"]
 
--- lakepowell/filter.py
+++ lakepowell/filter.py
@@ -6,18 +6,23 @@ class Filter:
         self.df = df.copy() #pandas data frame copied to avoid pointer problems
 
     # select columns of interest by name
+    # @Param cols: list of type string column names to be included in the new dataframe
     def selectColumns(self, cols):
         self.df = self.df[cols]
 
-    #filter based of of values in given list
+    #filter based on gear values in given list
+    # @Param gear: list of type string gear names to be included in the new dataframe
     def givenGear(self, gear):
         self.df = self.df[self.df.Gear.isin(gear)]
 
-    #filter based of of values in given list
+    #filter based on species values in given list
+    # @Param species: list of type string species names to be included in the new dataframe
     def givenSpecies(self, species):
         self.df = self.df[self.df.Species.isin(species)]
 
-    #male and female are True/False values
+    #Only includes fish that have the given male or female distinction
+    # @Param male: boolean for whether males should be included or not
+    # @Param male: boolean for whether females should be included or not
     def givenSexes(self, male, female):
         if male and female: #all data with Male or Female distinction
             self.df = self.df[self.df.Sex == "M" or self.df.Sex == "F"]
@@ -26,51 +31,72 @@ class Filter:
         elif not male and female: #only females
             self.df = self.df[self.df.Sex == "F"]
 
-    #filter based of of values in given list
+    #filter based on site values in given list
+    # @Param sites: list of type string site names to be included in the new dataframe
     def givenSites(self, sites):
         self.df = self.df[self.df.Site.isin(sites)]
 
-    #inclusive range
+    #returns fish that are in the length range of low (inclusive) to high (inclusive)
+    # @Param low: fish must be greater or equal to this value
+    # @Param high: fish must be less than or equal to this value
     def rangeLength(self, low, high):
         self.df = self.df[self.df.Length >= low]
         self.df = self.df[self.df.Length <= high]
 
-    #inclusive range
+    #returns fish that are in the mass range of low (inclusive) to high (inclusive)
+    # @Param low: fish must be greater or equal to this value
+    # @Param high: fish must be less than or equal to this value
     def rangeMass(self, low, high):
         self.df = self.df[self.df.Mass >= low]
         self.df = self.df[self.df.Mass <= high]
 
-    #inclusive range
+    #returns fish that are in the KTL range of low (inclusive) to high (inclusive)
+    # @Param low: fish must be greater or equal to this value
+    # @Param high: fish must be less than or equal to this value
     def rangeKtl(self, low, high):
         self.df = self.df[self.df.Ktl >= low]
         self.df = self.df[self.df.Ktl <= high]
 
-    #filter Year column between given years
+    #returns fish that are caught from year1 (inclusive) and year2 (inclusive)
+    # @Param year1: fish must be caught after or during this year (type integer)
+    # @Param year2: fish must be caught before or during this year (type integer)
     def rangeYears(self, year1, year2):
         self.df = self.df[self.df.Year >= year1]
         self.df = self.df[self.df.Year <= year2]
 
-    #filter Month column between given months
+    #returns fish that are caught from month1 (inclusive) and month2 (inclusive) in any year
+    # @Param year1: fish must be caught after or during this month (type integer)
+    # @Param year2: fish must be caught before or during this month (type integer)
     def rangeMonths(self, month1, month2):
         self.df = self.df[self.df.Month >= month1]
         self.df = self.df[self.df.Month <= month2]
 
-    #filter Day between given days
+    #returns fish that are caught from day1 (inclusive) and day2 (inclusive) in any month
+    # @Param year1: fish must be caught after or during this day (type integer)
+    # @Param year2: fish must be caught before or during this day (type integer)
     def rangeDays(self, day1, day2):
         self.df = self.df[self.df.Day >= day1]
         self.df = self.df[self.df.Day <= day2]
 
     #filter rows only between the given dates - will work with bad dates as long
     #as they are numbers
+    # @Param d1: fish must be caught after or during this day (type integer)
+    # @Param m1: fish must be caught after or during this month (type integer)
+    # @Param y1: fish must be caught after or during this year (type integer)
+    # @Param d2: fish must be caught before or during this day (type integer)
+    # @Param m2: fish must be caught after or during this month (type integer)
+    # @Param y2: fish must be caught after or during this year (type integer)
     def betweenDates(self, d1, m1, y1, d2, m2, y2):
         self.rangeYears(y1, y2)
         self.df = self.df[(self.df.Month >= m1) & (self.df.Day >= d1)]
         self.df = self.df[(self.df.Month <= m2) & (self.df.Day <= d2)]
 
-    #filter based on given months
+    #filter based on month values in given list
+    # @Param month: list of type integer of months to be included in the new dataframe
     def givenMonths(self, months):
         self.df = self.df[self.df.Month.isin(months)]
 
-    #filter based on given years
+    #filter based on year values in given list
+    # @Param month: list of type integer of years to be included in the new dataframe
     def givenYears(self, years):
         self.df = self.df[self.df.Year.isin(years)]
